40508076e9c7e90b00cfbe5eb7ed492b1c2e57ea
Aditya Jyoti <aj.adityajyoti@gmail.com>
2025-05-04 00:47:56 +0530

    update: Add content parameter

--- writer-backend/models/blog.go
+++ writer-backend/models/blog.go
@@ -8,6 +8,7 @@ type Blog struct {
 	Id          string    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
+	Content     string    `json:"content"`
 	Published   bool      `json:"published"`
 	PublishDate time.Time `json:"publish_date"`
 	UpdateDate  time.Time `json:"update_date"`
