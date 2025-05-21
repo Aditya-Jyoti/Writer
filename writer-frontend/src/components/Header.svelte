659c0d3cb6bcf9a2ce7941d68986070648b6b63a
Aditya Jyoti <aj.adityajyoti@gmail.com>
2025-05-21 14:31:05 +0530

    chore: Change navbar to link to my main site

--- writer-frontend/src/components/Header.svelte
+++ writer-frontend/src/components/Header.svelte
@@ -7,9 +7,6 @@
 
 	<div class="text-foreground hover:text-foreground flex gap-8">
 		<a href="/#root" class="text-foreground hover:text-foreground">Root</a>
-		<a href="/#projects" class="text-foreground hover:text-foreground">Projects</a>
-		<a href="/#whatido" class="text-foreground hover:text-foreground">What I Do</a>
-		<a href="/blog" class="text-foreground hover:text-foreground">Blog</a>
-		<a href="/#contact" class="text-foreground hover:text-foreground">Contact</a>
+		<a href="https://adityajyoti.com" class="text-foreground hover:text-foreground">Main Website</a>
 	</div>
 </nav>
